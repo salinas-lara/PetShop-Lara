8d1507a7fe539686f572e2c9e4c1f0262e59f934
Lara Salinas <l.salinas.if@gmail.com>
2026-03-18 08:24:58 -0300

    ajustando menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -2,6 +2,12 @@ import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
 	res.render('pages/page', {
+		menu: {
+			all:true,
+			dogs: false,
+			cats:false,
+			fishes:false,
+		},
 		banner: {
 			title: 'Todos os animais',
 			background: 'allanimals.jpg'
@@ -10,13 +16,46 @@ export const home = (req: Request, res: Response) => {
 };
 
 export const dogs = (req: Request, res: Response) => {
-	res.render('pages/page');
+	res.render('pages/page', {
+		menu: {
+			all:false,
+			dogs: true,
+			cats:false,
+			fishes:false,
+		},
+		banner: {
+			title: 'Cachorros',
+			background: 'banner_dog.jpg'
+		}
+	});
 };
 
 export const cats = (req: Request, res: Response) => {
-	res.render('pages/page');
+	res.render('pages/page', {
+		menu: {
+			all:false,
+			dogs: false,
+			cats:true,
+			fishes:false,
+		},
+		banner: {
+			title: 'Gatos',
+			background: 'banner_cat.jpg'
+		}
+	});
 };
 
 export const fishes = (req: Request, res: Response) => {
-	res.render('pages/page');
+	res.render('pages/page', {
+		menu: {
+			all:false,
+			dogs: false,
+			cats:false,
+			fishes:true,
+		},
+		banner: {
+			title: 'Peixes',
+			background: 'banner_fish.jpg'
+		}
+	});
 };
